cbde7bc6a86609a845d4f0835c377f5053973a91
Vikas Patel <vikaspatel07071999@gmail.com>
2024-07-02 12:47:53 +0530

    array string mapreducefilter

---
+++ G1/2nd july array and Functions/g1.js
@@ -0,0 +1,67 @@
+// let arr=[2,5,8];
+//   let newArray=arr.map((element,idx,arr)=>{
+//     // console.log(element,idx,arr);
+//     if(idx<2){
+//         return element*element;
+//     }
+//     else{
+//        return  element;
+//     }
+//   });
+
+//   console.log(newArray);
+
+// let arr=[2,6,3,9,8];
+//    let newArray=arr.filter((element,idx,arr)=>{
+//     // console.log(element,idx,arr);
+//     if(element%3==0){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+//    })
+
+//    console.log(newArray);
+
+// let arr = [2, 6, 3, 9, 8];
+// let newArray = arr.reduce((acc, curr, idx, arr) => {
+//     if (idx >= 2) {
+//       acc = acc + curr;
+//     }
+//     console.log(acc,curr,idx,arr);
+  
+//     return acc;
+//   }, 0);
+// console.log(newArray);
+
+// let ans=Math.max(2,5,8,22,12);
+// let ans=Math.min(2,5,8,22,12);
+
+// console.log(ans);
+
+// if(2==="2"){
+//     console.log("hello");
+// }
+// else{
+//     console.log("bye");
+// }
+
+
+// arr.push(23);
+// arr.pop();
+// arr.shift();
+// arr.unshift(100);
+let arr=[111,2,55,99,9];
+// let arr=["bd","ab","abc","a"];
+// arr.sort((a,b)=>{
+//     return a-b;
+// });
+// console.log(arr);
+
+
+
+let str="chITkaRA UnivERSIty";
+// let ans=str.toUpperCase();
+// let ans=str.toLowerCase();
+console.log(ans);
---
+++ G3/2 july array and HOF/g3.js
@@ -0,0 +1,40 @@
+// console.log("hello g3");
+// let arr=[2,3,7,5,21];
+
+// let newArray=arr.map((element,idx,arr)=>{
+// console.log(element,idx,arr);
+// return element*2
+// });
+
+// let newArray=arr.filter((element,idx,arr)=>{
+//     console.log(element,idx,arr);
+//     if(idx>2){
+//         if(element%3==0){
+//             return true;
+//           }
+//           else{
+//             return false;
+//           }
+//     }
+// });
+
+
+// let newArray=arr.reduce((acc,curr,idx,arr)=>{
+//     console.log(acc,curr,idx,arr);
+//     acc=acc+curr;
+//     return acc;
+// })
+// console.log(newArray);
+
+let arr=[3,4,8,2,1,2,6,8,9];
+console.log(arr);
+// arr.push(23);
+// arr.pop();
+// arr.shift();
+// arr.unshift(234);
+// let brr=arr.slice(2,4);
+ arr.splice(2,4);
+console.log(arr);
+
+582,547,528,561,575,603,572
+
